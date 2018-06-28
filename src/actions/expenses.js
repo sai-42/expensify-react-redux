586634b6fbaaff3b0c79892a9a0a3a4a5bb625cb
Sabrina Moraru <sabrina.moraru@gmail.com>
2018-06-28 16:37:49 +0300

    Add startEditExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -48,6 +48,15 @@ export const editExpense = (id, updates) => ({
   updates
 });
 
+// START_EDIT_EXPENSE
+export const startEditExpense = (id, updates) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).update(updates).then(() => {
+      dispatch(editExpense(id, updates));
+    });
+  };
+};
+
 // SET_EXPENSES
 export const setExpenses = (expenses) => ({
   type: 'SET_EXPENSES',
